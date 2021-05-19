eb8460c7d8d0775f5e8484a73ae38727240bd138
Istiackrohan <istiackrohan@gmail.com>
2021-05-19 23:33:23 +0600

    Blur korte partesi na background

---
+++ app.js
@@ -0,0 +1,5 @@
+const logInButton = document.getElementById("Log-in");
+logInButton.addEventListener("click", ()=>{
+    const loginArea = document.getElementById("login-container");
+    loginArea.style.display = "none";
+})
